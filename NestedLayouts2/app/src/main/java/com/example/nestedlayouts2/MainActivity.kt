43d58b1c1144757e126a158b49702e9d4f4d7b7e
greendolf <ghost_4452@bk.ru>
2024-10-17 21:50:24 +0700

    not so done

---
+++ NestedLayouts2/app/src/main/java/com/example/nestedlayouts2/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.nestedlayouts2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.nestedlayouts2.ui.theme.NestedLayouts2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NestedLayouts2Theme {
+                Components()
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun Components() {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly) {
+        TextRow()
+        TextColumn()
+        TextTriangle()
+    }
+}
+
+@Composable
+fun TextRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .background(Color.Cyan),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Text")
+        Text(text = "Text")
+        Text(text = "Text")
+    }
+}
+
+@Composable
+fun TextColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Text")
+        Text(text = "Text")
+        Text(text = "Text")
+    }
+}
+
+@Composable
+fun TextTriangle() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text(text = "Text", Modifier.padding(10.dp))
+            Text(text = "Text", Modifier.padding(10.dp))
+        }
+        Text(text = "Text")
+    }
+}
