737e06541cd0592e886fbf2b6902026825031f37
greendolf <ghost_4452@bk.ru>
2024-10-24 12:06:24 +0700

    init

--- src/main/kotlin/1.1.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    var x = readln()
-    var a = x.first().digitToInt()
-    var b = x.last().digitToInt()
-    var y = a + b
-    println(y)
-}
--- src/main/kotlin/11.1.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    var fahrenheit = { temp: Int -> (temp * 9 / 5) + 32 }
-    println(fahrenheit(0))
-    println(fahrenheit(5))
-    println(fahrenheit(10))
-}
